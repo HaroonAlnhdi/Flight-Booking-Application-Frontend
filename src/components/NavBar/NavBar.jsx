3bea475982a452138c98cb3db1603ca6da2170f2
Haroon <you@example.com>
2024-08-17 13:27:59 +0300

    add React Icons package and update Navbar Style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,41 +2,62 @@ import React from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import { AiOutlineHome, AiOutlineInfoCircle, AiOutlinePhone, AiOutlineUser, AiOutlineLogout } from 'react-icons/ai';
+
+
 const MyNavbar = ({ user, handleSignout }) => {
-  return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Navbar.Brand as={Link} to="/">
-        <img src="/pic/flight.jpg" alt="Logo" className='logo'/>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
-          {user ? (
-            <>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="" onClick={handleSignout}>
-                Sign Out
-              </Nav.Link>
-            </>
-          ) : (
-            <>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/signin">
-                Sign In
-              </Nav.Link>
-              <Nav.Link as={Link} to="/signup">
-                Sign Up
-              </Nav.Link>
-            </>
-          )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+ 
+    return (
+      <Navbar collapseOnSelect expand="lg" className=" bg-light bg-gradient">
+        <Navbar.Brand as={Link} to="/">
+          <img src="/pic/flight.jpg" alt="Logo" className='logo'/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/">
+                  <AiOutlineHome /> Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="#">
+                  <AiOutlineInfoCircle /> About
+                </Nav.Link>
+                <Nav.Link as={Link} to="#">
+                  <AiOutlinePhone /> Contact 
+                </Nav.Link>
+                <Nav.Link as={Link} to="#">
+                  <AiOutlineUser /> Profile 
+                </Nav.Link>
+                <Nav.Link as={Link} to="" onClick={handleSignout}>
+                  <AiOutlineLogout /> Sign Out
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/">
+                  <AiOutlineHome /> Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="#">
+                  <AiOutlineInfoCircle /> About
+                </Nav.Link>
+                <Nav.Link as={Link} to="#">
+                  <AiOutlinePhone /> Contact 
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signin">
+                  <AiOutlineUser /> Sign In
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  <AiOutlineUser /> Sign Up
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  };
+
+
 
 export default MyNavbar;
