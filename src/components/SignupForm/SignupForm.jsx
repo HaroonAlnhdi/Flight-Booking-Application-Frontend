e767ea1a5d29fe6b0503348f3821b3b3f548c1ea
Haroon <you@example.com>
2024-08-18 13:20:43 +0300

    Update SignupForm component with additional fields and styling

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -2,6 +2,7 @@ import authService from '../../services/authService'
 
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import './SignupForm.css';
 
 const SignupForm = (props) => {
   const navigate = useNavigate();
@@ -10,6 +11,10 @@ const SignupForm = (props) => {
     username: '',
     password: '',
     passwordConf: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone_number: '',
   });
 
   const updateMessage = (msg) => {
@@ -33,54 +38,112 @@ const SignupForm = (props) => {
     }
   };
 
-  const { username, password, passwordConf } = formData;
+  const { username, password, passwordConf,first_name,last_name,email,phone_number } = formData;
 
   const isFormInvalid = () => {
     return !(username && password && password === passwordConf);
   };
 
   return (
-    <main className='signMain'>
-      <h1>Sign Up</h1>
-      <p>{message}</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="name"
-            value={username}
-            name="username"
-            onChange={handleChange}
-          />
+    <main className='signupmain'>
+      <div className='leftSide'>
+        <video autoPlay loop muted playsInline className="backVideo">
+          <source src="https://cdnl.iconscout.com/lottie/premium/preview-watermark/online-ticket-booking-animation-download-in-lottie-json-gif-static-svg-file-formats--travel-flight-tourism-hotel-pack-holidays-animations-5105109.mp4" type="video/mp4" />
+        </video>
+
+
+      </div>
+      <div className='rightSide'>
+        <div className="SignupFormContainer">  
+          <h1>Sign Up</h1>
+          <p>{message}</p>
+          <form onSubmit={handleSubmit}>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={username}
+                  name="username"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  name="password"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="confirm">Confirm Password:</label>
+                <input
+                  type="password"
+                  id="confirm"
+                  value={passwordConf}
+                  name="passwordConf"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="first_name">First Name:</label>
+                <input
+                  type="text"
+                  id="first_name"
+                  value={first_name}
+                  name="first_name"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="last_name">Last Name:</label>
+                <input
+                  type="text"
+                  id="last_name"
+                  value={last_name}
+                  name="last_name"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone_number">Phone Number:</label>
+                <input
+                  type="text"
+                  id="phone_number"
+                  value={phone_number}
+                  name="phone_number"
+                  max={8}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  name="email"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className='signupbtn'>
+              <button disabled={isFormInvalid()}>Sign Up</button>
+            </div>
+            <p>You already have an account? <Link to="/signin">Login</Link></p>
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            name="password"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirm">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirm"
-            value={passwordConf}
-            name="passwordConf"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button disabled={isFormInvalid()}>Sign Up</button>
-          <Link to="/">
-            <button>Cancel</button>
-          </Link>
-        </div>
-      </form>
+      </div>
     </main>
   );
 };
