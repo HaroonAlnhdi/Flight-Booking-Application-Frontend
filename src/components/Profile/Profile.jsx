47f995c743b7dcfdf1262a316d99c7d858b4841b
Haroon <you@example.com>
2024-08-18 14:33:12 +0300

    Profile component to display user information

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,12 @@
 
 const profile = ({user}) => {
   return (
+    console.log(user),
     <div>
       <h1>{user._id}</h1>
+      <h2>{user.username}</h2>
+      <h3>{user.first_name}</h3>
+      <h4>{user.Last_name}</h4>
     </div>
   );
 }
