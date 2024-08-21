1da782bbe9da6deefce22a4f276a7a24db1e5944
ali-lutfalla <ali,latfalla@gmail.com>
2024-08-21 10:36:24 +0300

    added css for FAQ page and also added FAQ questions

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -5,29 +5,31 @@ const FAQ = () => {
         <>
 
         <section className="faq-container">
+            <h3>HAVE ANY QUESTIONS?</h3>
+            <h1>Frequently Asked Questions</h1>
          <Accordion>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Header>Can I book flights for multiple passengers in a single transaction?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Yes, our application allows you to book flights for multiple passengers in a single transaction.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>How can I modify or cancel a flight booking?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        You can't modify but you can cancel your flight booking by logging into your account, accessing your booking details, and following the prompts to make cancellation.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Can I view my booking history and upcoming flights in the application?</Accordion.Header>
+        <Accordion.Body>
+            yes you can access your booking history in the tickets button in the navigation bar to view your tickets information for the trips.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Can I book flights for multiple passengers in a single transaction?</Accordion.Header>
+        <Accordion.Body>
+            yes you can book how many tickets as you want regardless if it's for multiple passengers or not, you have the toatal freedom to pick what you want.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
